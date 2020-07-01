2de9b12ea49425036e7365e16f00adddb19fa385
DavidKLim <limddavid@gmail.com>
2020-07-01 11:17:38 -0400

    cl_agreement break condition

--- R/FSCseq.R
+++ R/FSCseq.R
@@ -1109,7 +1109,7 @@ EM_run <- function(ncores,X=NA, y, k,
           break
         }
       }
-    }else if(a>10){if(all(cl_agreement[2:10]==1) & Tau==1){
+    }else if(a>10){if(all(cl_agreement[(a-9):a]==1) & Tau==1){
       # if cls are not changing for a while
       finalwts<-wts
       break
